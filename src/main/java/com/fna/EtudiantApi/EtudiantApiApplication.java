2aabc336b2e44edd451f6b199d969e65f776deef
foudangaalex <foudangaalex@gmail.com>
2026-05-05 20:16:42 +0100

    first commit

---
+++ src/main/java/com/fna/EtudiantApi/EtudiantApiApplication.java
@@ -0,0 +1,13 @@
+package com.fna.EtudiantApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudiantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudiantApiApplication.class, args);
+	}
+
+}
