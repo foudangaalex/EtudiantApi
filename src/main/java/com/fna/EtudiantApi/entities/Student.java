5a6cefcfed4e97c5b1b17b0f95c83dedaa946721
foudangaalex <foudangaalex@gmail.com>
2026-05-08 11:03:31 +0100

    feat:add student.java ,002-insert-student-table-data to entiest and dd.changelog folder

---
+++ src/main/java/com/fna/EtudiantApi/entities/Student.java
@@ -0,0 +1,17 @@
+package com.fna.EtudiantApi.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter @ToString @Builder
+public class Student {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
